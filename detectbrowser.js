2aade437c324e172a1047cc721f4949c0ef55223
Lance Vo <lvo@vinavo.us>
2015-04-17 12:35:57 -0500

    update to support mobile/os

--- detectbrowser.js
+++ detectbrowser.js
@@ -3,10 +3,12 @@
  version: 0.01
 */
 
-function detectBrowser(ua){
-  var tmp, browser, version, index;
 
-  browser =  ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || "Other";
+
+function detectBrowser(ua){
+  var browser, version, index, os, mobile;
+
+  browser =  ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || ['','other'];
 
   browser = browser[1];
 
@@ -17,7 +19,7 @@ function detectBrowser(ua){
   }
 
   var tridentVersion = ua.match(/trident\/(\d+)/i),
-      safariVersion = ua.match(/Version\/(\d+)/);
+    safariVersion = ua.match(/Version\/(\d+)/);
 
   index = ua.indexOf(browser);
 
@@ -25,12 +27,27 @@ function detectBrowser(ua){
     version = parseInt(tridentVersion[1]) + 4;
   } else if (browser==='Safari' && safariVersion!=null) {
     version = safariVersion[1];
+  } else if (browser==='Opera' && safariVersion!=null) {
+    version = safariVersion[1];
   } else {
     version = parseInt(ua.substring(index + browser.length + 1));
   }
 
+  os = ua.match(/(windows|mac os x|linux|symbos)/i) || ['','other'];
+  os = os[1].toLowerCase();
+
+  /*
+    Notes: iPod ua includes iPhone
+
+   */
+  mobile = ua.match(/(ipod|ipad|iphone|android|opera mobi|mobile)/i) || ['',''];
+
+  mobile = mobile[1];
+
   return {
-    browser: browser,
-    version: version
+    name: browser.toLowerCase(),
+    version: version,
+    os : os.toLowerCase(),
+    mobile: mobile.toLowerCase()
   };
 }
